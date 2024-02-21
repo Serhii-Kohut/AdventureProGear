c43fc1b896dd294ca4281626c9f898924b295db1
FilosofDanil <serhio3347@gmail.com>
2024-02-21 14:45:15 +0100

    Initial commit

---
+++ src/main/java/com/example/adventureprogearjava/AdventureProGearJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.adventureprogearjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdventureProGearJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdventureProGearJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/adventureprogearjava/AdventureProGearJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.adventureprogearjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdventureProGearJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
