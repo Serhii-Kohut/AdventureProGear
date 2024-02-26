db0adc0b0a179cacdb5e690bca9463533d18466c
Serhii Kohut <113467605+ronycv@users.noreply.github.com>
2024-02-26 21:53:14 +0200

    added constraints for fields in UserDTO

--- src/main/java/com/example/adventureprogearjava/dto/UserDTO.java
+++ src/main/java/com/example/adventureprogearjava/dto/UserDTO.java
@@ -1,6 +1,10 @@
 package com.example.adventureprogearjava.dto;
 
 import com.example.adventureprogearjava.entity.enums.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Pattern;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -14,11 +18,23 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class UserDTO {
+
+    @NotBlank(message = "Name is mandatory")
     String name;
+
+    @NotBlank(message = "Surname is mandatory")
     String surname;
+
+    @Email(message = "Email should be valid")
     String email;
+
+    @Pattern(regexp="(^$|[0-9]{10})", message="Phone number should be valid")
     String phoneNumber;
+
     boolean verified;
+
+    @PastOrPresent(message = "Date should be in the past or present")
     LocalDate date;
+
     Role role;
 }
