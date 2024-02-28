2da68d7c0cd70a45501b210af045d3ca654c51ab
Serhii Kohut <113467605+ronycv@users.noreply.github.com>
2024-02-28 19:10:47 +0200

    added UserServiceImplTest

---
+++ src/test/java/com/example/adventureprogearjava/services/impl/UserServiceImplTest.java
@@ -0,0 +1,105 @@
+package com.example.adventureprogearjava.services.impl;
+
+import com.example.adventureprogearjava.dto.UserDTO;
+import com.example.adventureprogearjava.entity.User;
+import com.example.adventureprogearjava.repositories.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+import static org.mockito.internal.verification.VerificationModeFactory.times;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserServiceImplTest {
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private UserServiceImpl userService;
+
+    private User user;
+
+    @BeforeEach
+    public void setUp() {
+        user = new User();
+        user.setId(1L);
+        user.setName("Test User");
+    }
+
+    @Test
+    public void testGetAll() {
+        List<User> users = new ArrayList<>();
+        users.add(user);
+        when(userRepository.findAll()).thenReturn(users);
+
+        List<UserDTO> result = userService.getAll();
+
+        verify(userRepository, times(1)).findAll();
+        assertFalse(result.isEmpty());
+        assertEquals(users.size(), result.size());
+    }
+
+    @Test
+    public void testGetById() {
+        when(userRepository.findById(1L)).thenReturn(Optional.of(user));
+
+        UserDTO result = userService.getById(1L);
+
+        assertNotNull(result);
+        assertEquals(user.getName(), result.getName());
+
+        verify(userRepository, times(1)).findById(1L);
+    }
+
+    @Test
+    public void testCreate() {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setName("Test User");
+        when(userRepository.existsByEmail(anyString())).thenReturn(false);
+        when(userRepository.existsByPhoneNumber(anyString())).thenReturn(false);
+        when(userRepository.save(any(User.class))).thenReturn(user);
+
+        UserDTO result = userService.create(userDTO);
+
+        verify(userRepository, times(1)).save(any(User.class));
+        assertNotNull(result);
+        assertEquals(user.getName(), result.getName());
+    }
+
+    @Test
+    public void testUpdate() {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setName("Updated Name");
+        when(userRepository.findById(anyLong())).thenReturn(Optional.of(user));
+        when(userRepository.save(any(User.class))).thenReturn(user);
+
+        userService.update(userDTO, 1L);
+
+        verify(userRepository, times(1)).findById(anyLong());
+        verify(userRepository, times(1)).save(any(User.class));
+    }
+
+    @Test
+    public void testDelete() {
+        doNothing().when(userRepository).deleteById(anyLong());
+
+        userService.delete(1L);
+
+        verify(userRepository, times(1)).deleteById(anyLong());
+    }
+
+}
