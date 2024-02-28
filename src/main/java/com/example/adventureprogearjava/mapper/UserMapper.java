7fb0a389ef8f865a295ab8a2c4a2b23970eb37b3
Serhii Kohut <113467605+ronycv@users.noreply.github.com>
2024-02-28 16:14:10 +0200

    UserMapper: added MAPPER
    UserRepository: added methods existsByEmail and existsByPhoneNumber

--- src/main/java/com/example/adventureprogearjava/mapper/UserMapper.java
+++ src/main/java/com/example/adventureprogearjava/mapper/UserMapper.java
@@ -3,9 +3,12 @@ package com.example.adventureprogearjava.mapper;
 import com.example.adventureprogearjava.dto.UserDTO;
 import com.example.adventureprogearjava.entity.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface UserMapper {
+    UserMapper MAPPER = Mappers.getMapper(UserMapper.class);
+
     public UserDTO toDTO(User user);
 
     public User toEntity(UserDTO dto);
