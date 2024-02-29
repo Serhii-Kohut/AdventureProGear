f80403206653cf617a1e03bd653db4173d81a1fb
FilosofDanil <serhio3347@gmail.com>
2024-02-29 10:39:38 +0100

    CRUDServices: - Added tests for ProductContentService - Fixed minor issues

--- src/main/java/com/example/adventureprogearjava/services/impl/ProductContentServiceImpl.java
+++ src/main/java/com/example/adventureprogearjava/services/impl/ProductContentServiceImpl.java
@@ -14,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -48,6 +49,7 @@ public class ProductContentServiceImpl implements CRUDService<ContentDTO> {
     }
 
     @Override
+    @Transactional
     public ContentDTO create(ContentDTO contentDTO) {
         log.info("Adding new content.");
         contentRepository.insertContent(contentDTO.getSource(),
@@ -56,6 +58,7 @@ public class ProductContentServiceImpl implements CRUDService<ContentDTO> {
     }
 
     @Override
+    @Transactional
     public void update(ContentDTO contentDTO, Long id) {
         log.info("Updating content with id: {}", id);
         ProductContent existingContent = contentRepository.findById(id)
---
+++ src/test/resources/create_product_content_before.sql
@@ -0,0 +1,3 @@
+insert into products (id, product_name, description, base_price, category)
+values (6, 'Test', 'Test', 1, 'HIKING_EQUIPMENT');
+insert into product_content (id, product_id, source) VALUES (11, 6, 'img_6_1')
