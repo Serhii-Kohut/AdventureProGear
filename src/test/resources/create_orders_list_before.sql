75536a470ca912f3b75e5f1a0735825222b96616
Serhii Kohut <113467605+ronycv@users.noreply.github.com>
2024-03-02 17:44:56 +0200

    fixed sql-scripts
    added sql-script delete_orders_list_after_create.sql

--- src/test/resources/create_orders_list_before.sql
+++ src/test/resources/create_orders_list_before.sql
@@ -1,11 +1,11 @@
 INSERT INTO orders (id, user_id, city, post_address, comment, price, status)
-VALUES (55, 1, 'city', 'TestAddress', 'TestComment', 100, 'DELIVERED');
+VALUES (45, 1, 'city', 'TestAddress', 'TestComment', 100, 'DELIVERED');
 
 INSERT INTO products (id, product_name, description, base_price, category)
-VALUES (88, 'Test', 'Test', 1, 'HIKING_EQUIPMENT');
+VALUES (48, 'Test', 'Test', 1, 'HIKING_EQUIPMENT');
 
 INSERT INTO product_attributes (id, size, color, price_deviation, product_id, quantity)
-VALUES (32, 'M', 'green', 0, 88, 1);
+VALUES (42, 'M', 'green', 0, 4, 1);
 
 INSERT INTO orders_list (id, order_id, product_id, product_attribute_id, quantity)
-VALUES (88, 55, 88, 32, 1);
+VALUES (88, 45, 48, 42, 1);
