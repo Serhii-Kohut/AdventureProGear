6343f95108459a850e768261532dd4922a2a050a
Serhii Kohut <113467605+ronycv@users.noreply.github.com>
2024-03-04 21:56:10 +0200

    added UserController

---
+++ src/main/java/com/example/adventureprogearjava/controller/UserController.java
@@ -0,0 +1,57 @@
+package com.example.adventureprogearjava.controller;
+
+import com.example.adventureprogearjava.dto.UserDTO;
+import com.example.adventureprogearjava.services.impl.CRUDUserServiceImpl;
+import jakarta.validation.Valid;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class UserController {
+
+    CRUDUserServiceImpl crudUserService;
+
+    @GetMapping
+    public List<UserDTO> getAllUsers() {
+        return crudUserService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public UserDTO getUserById(@PathVariable Long id) {
+        return crudUserService.getById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserDTO createUser(@Valid @RequestBody UserDTO userDTO) {
+        return crudUserService.create(userDTO);
+    }
+
+    @PutMapping("/{id}")
+    public void updateUser(@Valid @RequestBody UserDTO userDTO, @PathVariable Long id) {
+        crudUserService.update(userDTO, id);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteUser(@PathVariable Long id) {
+        crudUserService.delete(id);
+    }
+
+}
