d5c49b609f2ec4352213a12dbf506b5ad871e5a0
FilosofDanil <serhio3347@gmail.com>
2024-03-05 09:28:42 +0100

    Annotated exceptions with ResponseStatus.

--- src/main/java/com/example/adventureprogearjava/exceptions/UserAlreadyExistsException.java
+++ src/main/java/com/example/adventureprogearjava/exceptions/UserAlreadyExistsException.java
@@ -1,5 +1,9 @@
 package com.example.adventureprogearjava.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class UserAlreadyExistsException extends RuntimeException{
     public UserAlreadyExistsException(String message) {
         super(message);
