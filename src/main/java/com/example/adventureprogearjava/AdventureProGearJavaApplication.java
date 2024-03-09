4ea276e88f49b2a861d9257bd58c49fbd42cef0a
FilosofDanil <serhio3347@gmail.com>
2024-03-09 08:51:11 +0100

    Fixed swagger issue

--- src/main/java/com/example/adventureprogearjava/AdventureProGearJavaApplication.java
+++ src/main/java/com/example/adventureprogearjava/AdventureProGearJavaApplication.java
@@ -2,8 +2,10 @@ package com.example.adventureprogearjava;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class AdventureProGearJavaApplication {
 
     public static void main(String[] args) {
