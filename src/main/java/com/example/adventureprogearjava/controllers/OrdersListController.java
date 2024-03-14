e219bc7813a45bd612e7e241ea73e258b2b4313e
FilosofDanil <serhio3347@gmail.com>
2024-03-14 20:53:05 +0100

    Added documentation for OrdersListController

--- src/main/java/com/example/adventureprogearjava/controllers/OrdersListController.java
+++ src/main/java/com/example/adventureprogearjava/controllers/OrdersListController.java
@@ -2,20 +2,18 @@ package com.example.adventureprogearjava.controllers;
 
 import com.example.adventureprogearjava.dto.OrdersListDTO;
 import com.example.adventureprogearjava.services.impl.CRUDOrdersListServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,33 +21,118 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping("/api/order-lists")
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@Tag(name = "Order Controller",
+        description = "API operations with order lists")
 public class OrdersListController {
 
     CRUDOrdersListServiceImpl ordersListService;
 
     @GetMapping
+    @Operation(
+            summary = "Get all order lists",
+            description = "Retrieves all available order lists. Order list contains info about " +
+                    "products in order."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Successful operation",
+            content = @Content(schema = @Schema(implementation = OrdersListDTO.class))
+    )
     public List<OrdersListDTO> getAllOrderLists() {
         return ordersListService.getAll();
     }
 
     @GetMapping("/{id}")
-    public OrdersListDTO getOrderListById(@PathVariable Long id) {
+    @Operation(
+            summary = "Get order by it's own id",
+            description = "Retrieves order list by id"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Successful operation",
+            content = @Content(schema = @Schema(implementation = OrdersListDTO.class))
+    )
+    @ApiResponse(
+            responseCode = "404",
+            description = "Not Found",
+            content = @Content(schema = @Schema(implementation = String.class))
+    )
+    public OrdersListDTO getOrderListById(@Parameter(
+            description = "ID of the order",
+            required = true
+    ) @PathVariable Long id) {
         return ordersListService.getById(id);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public OrdersListDTO createOrderList(@Valid @RequestBody OrdersListDTO ordersListDTO) {
+    @ApiResponse(
+            responseCode = "201",
+            description = "Successful operation.",
+            content = @Content(schema = @Schema(implementation = OrdersListDTO.class))
+    )
+    @ApiResponse(
+            responseCode = "400",
+            description = "Invalid data",
+            content = @Content(schema = @Schema(implementation = String.class))
+    )
+    @Operation(
+            summary = "Creation of new order list",
+            description = "Creation of new  order list."
+    )
+    public OrdersListDTO createOrderList(@io.swagger.v3.oas.annotations.parameters.RequestBody(
+            description = "Order list data, required for creation",
+            required = true,
+            content = @Content(schema = @Schema(implementation = OrdersListDTO.class))
+    )
+                                         @Valid @RequestBody OrdersListDTO ordersListDTO) {
         return ordersListService.create(ordersListDTO);
     }
 
     @PutMapping("/{id}")
-    public void updateOrderList(@Valid @RequestBody OrdersListDTO ordersListDTO, @PathVariable Long id) {
+    @ApiResponse(
+            responseCode = "200",
+            description = "Successful operation."
+    )
+    @ApiResponse(
+            responseCode = "404",
+            description = "Not Found",
+            content = @Content(schema = @Schema(implementation = String.class))
+    )
+    @ApiResponse(
+            responseCode = "400",
+            description = "Invalid data",
+            content = @Content(schema = @Schema(implementation = String.class))
+    )
+    @Operation(
+            summary = "Update of the order list",
+            description = "Update of the order list"
+    )
+    public void updateOrderList(
+            @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Order list data, required for creation",
+                    required = true,
+                    content = @Content(schema = @Schema(implementation = OrdersListDTO.class))
+            ) @Valid @RequestBody OrdersListDTO ordersListDTO,
+            @PathVariable Long id) {
         ordersListService.update(ordersListDTO, id);
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Operation(
+            summary = "Deleting order lists by it's own id",
+            description = "Deleting order lists by it's own id"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Successful operation."
+    )
+    @ApiResponse(
+            responseCode = "204",
+            description = "No content present.",
+            content = @Content(schema = @Schema(implementation = String.class))
+    )
     public void deleteOrderList(@PathVariable Long id) {
         ordersListService.delete(id);
     }
