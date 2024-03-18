eaa222bbe034353ad0bbd4467a640a4774205a33
Serhii Kohut <113467605+ronycv@users.noreply.github.com>
2024-03-18 09:35:27 +0200

    created UserUpdateDTO and PasswordUpdateDTO

---
+++ src/main/java/com/example/adventureprogearjava/dto/UserUpdateDTO.java
@@ -0,0 +1,24 @@
+package com.example.adventureprogearjava.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class UserUpdateDTO {
+    String name;
+    String surname;
+
+    @Email(message = "Email should be valid")
+    String email;
+
+    @Pattern(regexp = "(^$|\\+380[0-9]{9})", message = "Phone number should be valid")
+    String phoneNumber;
+}
