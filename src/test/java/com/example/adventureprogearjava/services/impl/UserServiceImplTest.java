f93f151fd8a252262bcef938dfff9e1606d3fa7f
Serhii Kohut <113467605+ronycv@users.noreply.github.com>
2024-03-21 20:34:41 +0200

    created VerificationTokenServiceImplTest

--- src/test/java/com/example/adventureprogearjava/services/impl/UserServiceImplTest.java
+++ src/test/java/com/example/adventureprogearjava/services/impl/UserServiceImplTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
