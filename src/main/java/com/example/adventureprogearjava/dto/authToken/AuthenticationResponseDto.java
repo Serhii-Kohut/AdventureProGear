d3a99169eae446baedf9e7e1d738d1acc56d36c7
Serhii Kohut <113467605+ronycv@users.noreply.github.com>
2024-03-28 21:02:58 +0200

    created DTO's for authentication and refreshing token.

---
+++ src/main/java/com/example/adventureprogearjava/dto/authToken/AuthenticationResponseDto.java
@@ -0,0 +1,20 @@
+package com.example.adventureprogearjava.dto.authToken;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class AuthenticationResponseDto {
+    String accessToken;
+
+    String refreshToken;
+
+}
