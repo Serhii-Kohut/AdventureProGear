90528940f38ad1442ce9380165c3d8a006b02dbc
FilosofDanil <serhio3347@gmail.com>
2024-03-28 18:07:51 +0100

    Minor fixes

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,8 @@ WORKDIR /app
 
 COPY target/AdventureProGearJava-0.0.1-SNAPSHOT.jar /app/pro.jar
 
+COPY uploads/ /app/uploads/
+
 EXPOSE 8080
 
 CMD ["java", "-jar", "pro.jar"]
--- src/main/java/com/example/adventureprogearjava/config/WebConfig.java
+++ src/main/java/com/example/adventureprogearjava/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000/", "http://localhost:8080", "https://adventure-pro-gear.vercel.app", "https://towering-house-production.up.railway.app")
+                        .allowedOrigins("http://localhost:5173", "http://localhost:3000", "http://localhost:8080", "https://adventure-pro-gear.vercel.app", "https://towering-house-production.up.railway.app")
                         .allowedMethods("*")
                         .allowedHeaders("*")
                         .allowCredentials(true);
