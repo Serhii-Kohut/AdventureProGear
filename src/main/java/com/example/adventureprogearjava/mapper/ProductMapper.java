f14a7dad62451ffde1facc957082ded11c6000c5
FilosofDanil <serhio3347@gmail.com>
2024-03-29 10:41:12 +0100

    Added link mapping for DTOs.

--- src/main/java/com/example/adventureprogearjava/mapper/ProductMapper.java
+++ src/main/java/com/example/adventureprogearjava/mapper/ProductMapper.java
@@ -1,17 +1,15 @@
 package com.example.adventureprogearjava.mapper;
 
-import com.example.adventureprogearjava.dto.ContentDTO;
 import com.example.adventureprogearjava.dto.ProductDTO;
 import com.example.adventureprogearjava.entity.Product;
-import com.example.adventureprogearjava.entity.ProductContent;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.springframework.stereotype.Component;
-
-import java.util.List;
+import org.mapstruct.Named;
 
 @Mapper(componentModel = "spring")
 public interface ProductMapper {
+    String api = "towering-house-production.up.railway.app/api/v1/products/";
+
     @Mapping(target = "productName", source = "product.productName")
     @Mapping(target = "description", source = "product.description")
     @Mapping(target = "basePrice", source = "product.basePrice")
@@ -19,6 +17,7 @@ public interface ProductMapper {
     @Mapping(target = "category", source = "product.category")
     @Mapping(target = "attributes", source = "product.attributes")
     @Mapping(target = "contents", source = "product.contents")
+    @Mapping(target = "selfLink", source = "product.id", qualifiedByName = "idToLink")
     ProductDTO toDto(Product product);
 
     @Mapping(target = "productName", source = "dto.productName")
@@ -27,4 +26,9 @@ public interface ProductMapper {
     @Mapping(target = "gender", source = "dto.gender")
     @Mapping(target = "category", source = "dto.category")
     Product toEntity(ProductDTO dto);
+
+    @Named("idToLink")
+    default String getLink(Long id) {
+        return api + id;
+    }
 }
