78ea361688e3d688a2f46a1a04e1ad2fae19f7e0
Serhii Kohut <113467605+ronycv@users.noreply.github.com>
2024-04-03 21:06:04 +0300

    fixed UserResponseDto

--- src/main/java/com/example/adventureprogearjava/dto/registrationDto/UserResponseDto.java
+++ src/main/java/com/example/adventureprogearjava/dto/registrationDto/UserResponseDto.java
@@ -1,6 +1,7 @@
 package com.example.adventureprogearjava.dto.registrationDto;
 
 import com.example.adventureprogearjava.entity.enums.Role;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -27,4 +28,16 @@ public class UserResponseDto {
     Role role;
 
     boolean verified;
+
+    @JsonIgnore
+    String password;
+
+    public UserResponseDto(Long id, String name, String surname, String email, Role role, boolean verified) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.role = role;
+        this.verified = verified;
+    }
 }
