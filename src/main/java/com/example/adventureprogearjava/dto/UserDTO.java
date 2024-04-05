13638ed909ec36aaca4956a872a31704c7c5e2be
Serhii Kohut <113467605+ronycv@users.noreply.github.com>
2024-04-05 19:47:43 +0300

    minor fix

--- src/main/java/com/example/adventureprogearjava/dto/UserDTO.java
+++ src/main/java/com/example/adventureprogearjava/dto/UserDTO.java
@@ -16,7 +16,6 @@ import lombok.experimental.FieldDefaults;
 import java.time.LocalDate;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class UserDTO {
