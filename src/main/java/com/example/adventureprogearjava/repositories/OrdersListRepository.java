e18a45e33873a43fe5afa4acbfb3369c5282bb33
Serhii Kohut <113467605+ronycv@users.noreply.github.com>
2024-06-29 17:47:45 +0300

    fixed 'order_list_seq' in 'OrdersListRepository'

--- src/main/java/com/example/adventureprogearjava/repositories/OrdersListRepository.java
+++ src/main/java/com/example/adventureprogearjava/repositories/OrdersListRepository.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Repository;
 public interface OrdersListRepository extends JpaRepository<OrdersList, Long> {
     @Modifying
     @Query(value = "insert into orders_list (id, order_id, product_id, product_attribute_id, quantity) " +
-            "values (nextval('orders_list_seq'), :orderId, :productId, :productAttributeId, :quantity)",
+            "values (nextval('order_list_seq'), :orderId, :productId, :productAttributeId, :quantity)",
             nativeQuery = true)
     void insertOrdersList(@Param("orderId") Long orderId,
                           @Param("productId") Long productId,
