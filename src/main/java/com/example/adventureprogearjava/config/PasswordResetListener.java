2353e42660a8ffb63b823ac5119c3891aaac176c
Serhii Kohut <113467605+ronycv@users.noreply.github.com>
2024-07-09 19:04:46 +0300

    changed server's URL in 'PasswordResetListener'

--- src/main/java/com/example/adventureprogearjava/config/PasswordResetListener.java
+++ src/main/java/com/example/adventureprogearjava/config/PasswordResetListener.java
@@ -31,7 +31,7 @@ public class PasswordResetListener implements ApplicationListener<OnPasswordRese
 
         String subject = environment.getProperty("email.reset.subject", "Password Reset Request");
 
-        String url = "https://incredible-creation-production.up.railway.app/reset-password?token=" + token;
+        String url = "http://localhost:3000/uk-UA/reset-password?token=" + token;
 
         String message = environment.getProperty("email.reset.body", "To reset your password, click the following link:") + "\r\n" + url;
 
