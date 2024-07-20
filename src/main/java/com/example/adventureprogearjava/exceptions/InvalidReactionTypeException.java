4b9f730febf60aef5083900de88615781e4ec9d6
Serhii Kohut <113467605+ronycv@users.noreply.github.com>
2024-07-20 16:49:00 +0300

    added 'InvalidReactionTypeException'

---
+++ src/main/java/com/example/adventureprogearjava/exceptions/InvalidReactionTypeException.java
@@ -0,0 +1,7 @@
+package com.example.adventureprogearjava.exceptions;
+
+public class InvalidReactionTypeException extends RuntimeException {
+    public InvalidReactionTypeException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/example/adventureprogearjava/repositories/ReactionToPostRepository.java
+++ src/main/java/com/example/adventureprogearjava/repositories/ReactionToPostRepository.java
@@ -4,13 +4,37 @@ import com.example.adventureprogearjava.entity.Post;
 import com.example.adventureprogearjava.entity.ReactionToPost;
 import com.example.adventureprogearjava.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ReactionToPostRepository extends JpaRepository<ReactionToPost, Long> {
+
+    @Modifying
+    @Transactional
+    @Query(value = "INSERT INTO reactions (id, type, post_id, user_id) " +
+            "VALUES (nextval('reaction_seq'), CAST(:reactionType AS reaction_type), :postId, :userId)",
+            nativeQuery = true)
+    void insertReaction(@Param("reactionType") String reactionType,
+                        @Param("postId") Long postId,
+                        @Param("userId") Long userId);
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE reactions SET type = CAST(:reactionType AS reaction_type), post_id = :postId, " +
+            "user_id = :userId WHERE id = :id", nativeQuery = true)
+    void updateReaction(@Param("id") Long id,
+                        @Param("reactionType") String reactionType,
+                        @Param("postId") Long postId,
+                        @Param("userId") Long userId);
+
+
     Optional<ReactionToPost> findByPostAndUser(Post post, User user);
 
     List<ReactionToPost> findByPost(Post post);
