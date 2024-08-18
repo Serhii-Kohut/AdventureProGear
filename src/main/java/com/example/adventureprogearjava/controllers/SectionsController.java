1bba35d4cd3b7009c953ec28168ff40a07662cb6
Viacheslav Bolshov <137940809+Bolshov92@users.noreply.github.com>
2024-08-18 10:50:08 +0100

    Bugfix #5 sections controller (#46)
    * Resolved merge conflicts
    
    * minor update
    
    * bug/fix/productAttributeController :
    Add security checks and custom access denied handler for ProductAttributeController
     Updated `SecurityConfig` to include JWT authentication filter and custom access denied handler for better security.
     Implemented `CustomAccessDeniedHandler` to return a specific error message when a non-admin tries to create a product attribute.
     Enhanced `ProductAttributeController` with `@PreAuthorize` annotation to restrict creation of product attributes to admin users only.
    
    * feat(FileController): Add access control for file operations
    
    - Added @PreAuthorize annotations to restrict access to file upload and download methods.
    - Introduced custom access denied message for non-admin users attempting to perform these actions.
    - Enhanced security by ensuring only users with the 'ROLE_ADMIN' authority can upload or download files.
    - Added a new FileAccessDeniedException class to handle access denial for file operations.
    
    * Added access control to createSection method: Only admins can create sections
    
    * fix Section Controller
    
    * Add methods in ProductController to support filtering by priceTo and priceFrom separately
    
    - Implemented new methods in `ProductServiceImpl` to handle these specific price filters.
    
    * Added method to send order confirmation emails. The method retrieves the saved order from the database, constructs a detailed order confirmation message including order details and items, and sends it to the user's email.
    
    * - Added CustomAccessDeniedHandler to handle all access denials with a unified "Access denied" message.
    - Removed custom exceptions for access denials from controllers.
    - Added constructor in FileService for proper dependency injection.
    
    * delete testing file for upload controller
    
    * Added Enum values ACCEPTED, SENT to OrderStatus;
    Updated message text in sendOrderConfirmation method
    
    * fix fileController
    
    * Resolved merge conflicts
    
    * Resolved merge conflicts
    
    * Resolved merge conflicts
    
    * Resolved merge conflicts
    
    * Resolved merge conflicts
    
    * Resolved merge conflicts
    
    * Resolved merge conflicts
    
    * Added admin permissions for POST, PUT, DELETE methods on /api/public/sections. Configured AccessDeniedException with access denied message
    
    * Added admin permissions for POST, PUT, DELETE methods on /api/public/sections. Configured AccessDeniedException with access denied message
    
    * Added admin permissions for POST, PUT, DELETE methods on /api/public/sections. Configured AccessDeniedException with access denied message
    
    * Added ADMIN rights to POST, PUT, DELETE methods in SectionsController
    
    ---------
    
    Co-authored-by: Berkovskyi Danylo <93262349+FilosofDanil@users.noreply.github.com>

--- src/main/java/com/example/adventureprogearjava/controllers/SectionsController.java
+++ src/main/java/com/example/adventureprogearjava/controllers/SectionsController.java
@@ -1,7 +1,6 @@
 package com.example.adventureprogearjava.controllers;
 
 import com.example.adventureprogearjava.dto.SectionDTO;
-import com.example.adventureprogearjava.exceptions.SectionsAccessDeniedException;
 import com.example.adventureprogearjava.services.CRUDService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AccessLevel;
@@ -10,8 +9,6 @@ import lombok.experimental.FieldDefaults;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -37,11 +34,7 @@ public class SectionsController {
 
     @PostMapping("")
     @PreAuthorize("hasAuthority('ROLE_ADMIN')")
-    public ResponseEntity<String> createSection(@RequestBody SectionDTO sectionDTO) throws SectionsAccessDeniedException {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        if (!auth.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ROLE_ADMIN"))) {
-            throw new SectionsAccessDeniedException("Only admins are allowed to create sections.");
-        }
+    public ResponseEntity<String> createSection(@RequestBody SectionDTO sectionDTO) {
         try {
             crudService.create(sectionDTO);
             return ResponseEntity.status(HttpStatus.OK).body("Section created successfully");
@@ -51,12 +44,14 @@ public class SectionsController {
     }
 
     @PutMapping("/{id}")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public void updateSection(@PathVariable("id") Long id,
                               @RequestBody SectionDTO sectionDTO){
         crudService.update(sectionDTO, id);
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public void deleteSection(@PathVariable("id") Long id){
         crudService.delete(id);
     }
