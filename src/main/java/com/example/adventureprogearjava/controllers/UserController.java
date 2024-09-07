f30e73eef8b402a11e6221d647711b85d270ccb8
Viacheslav Bolshov <137940809+Bolshov92@users.noreply.github.com>
2024-09-07 10:13:16 +0100

    User controller documentation (#60)
    * Resolved merge conflicts
    
    * minor update
    
    * bug/fix/productAttributeController :
    Add security checks and custom access denied handler for ProductAttributeController
     Updated `SecurityConfig` to include JWT authentication filter and custom access denied handler for better security.
     Implemented `CustomAccessDeniedHandler` to return a specific error message when a non-admin tries to create a product attribute.
     Enhanced `ProductAttributeController` with `@PreAuthorize` annotation to restrict creation of product attributes to admin users only.
    
    * feat(FileController): Add access control for file operations
    
    - Added @PreAuthorize annotations to restrict access to file upload and download methods.
    - Introduced custom access denied message for non-admin users attempting to perform these actions.
    - Enhanced security by ensuring only users with the 'ROLE_ADMIN' authority can upload or download files.
    - Added a new FileAccessDeniedException class to handle access denial for file operations.
    
    * Added access control to createSection method: Only admins can create sections
    
    * fix Section Controller
    
    * Add methods in ProductController to support filtering by priceTo and priceFrom separately
    
    - Implemented new methods in `ProductServiceImpl` to handle these specific price filters.
    
    * Added method to send order confirmation emails. The method retrieves the saved order from the database, constructs a detailed order confirmation message including order details and items, and sends it to the user's email.
    
    * - Added CustomAccessDeniedHandler to handle all access denials with a unified "Access denied" message.
    - Removed custom exceptions for access denials from controllers.
    - Added constructor in FileService for proper dependency injection.
    
    * delete testing file for upload controller
    
    * Added Enum values ACCEPTED, SENT to OrderStatus;
    Updated message text in sendOrderConfirmation method
    
    * fix fileController
    
    * Resolved merge conflicts
    
    * Resolved merge conflicts
    
    * Resolved merge conflicts
    
    * Resolved merge conflicts
    
    * Resolved merge conflicts
    
    * Resolved merge conflicts
    
    * Resolved merge conflicts
    
    * add custom annotations for User Controller , also
    disabled FileController
    
    ---------
    
    Co-authored-by: Berkovskyi Danylo <93262349+FilosofDanil@users.noreply.github.com>

--- src/main/java/com/example/adventureprogearjava/controllers/UserController.java
+++ src/main/java/com/example/adventureprogearjava/controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.example.adventureprogearjava.controllers;
 
+import com.example.adventureprogearjava.annotation.userController.*;
 import com.example.adventureprogearjava.dto.PasswordUpdateDTO;
 import com.example.adventureprogearjava.dto.UserCreateDTO;
 import com.example.adventureprogearjava.dto.UserDTO;
@@ -7,28 +8,16 @@ import com.example.adventureprogearjava.dto.UserUpdateDTO;
 import com.example.adventureprogearjava.dto.registrationDto.UserEmailDto;
 import com.example.adventureprogearjava.entity.User;
 import com.example.adventureprogearjava.services.impl.UserServiceImpl;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -43,155 +32,43 @@ public class UserController {
 
     UserServiceImpl crudUserService;
 
-    @GetMapping("/all")
-    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
-    @Operation(
-            summary = "Get all users. Available for ADMIN.",
-            description = "Retrieves all available users."
-    )
-    @ApiResponse(
-            responseCode = "200",
-            description = "Successful operation",
-            content = @Content(schema = @Schema(implementation = UserDTO.class))
-    )
+    @GetAllUsers(path = "/all")
     public List<UserDTO> getAllUsers() {
         return crudUserService.getAll();
     }
 
-    @GetMapping("/me")
-    @Operation(
-            summary = "Get info about me",
-            description = "Retrieves info about me"
-    )
-    @ApiResponse(
-            responseCode = "200",
-            description = "Successful operation",
-            content = @Content(schema = @Schema(implementation = UserDTO.class))
-    )
-    @ApiResponse(
-            responseCode = "404",
-            description = "Not Found",
-            content = @Content(schema = @Schema(implementation = String.class))
-    )
-    public UserDTO getUser(@Parameter(
-            description = "Info about me",
-            required = true
-    ) @AuthenticationPrincipal User user) {
+    @GetMe(path = "/me")
+    public UserDTO getUser(@AuthenticationPrincipal User user) {
         return crudUserService.getById(user.getId());
     }
 
-    @GetMapping("/{id}")
-    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
-    @Operation(
-            summary = "Get user by id. Available for ADMIN.",
-            description = "Retrieves User by id"
-    )
-    @ApiResponse(
-            responseCode = "200",
-            description = "Successful operation",
-            content = @Content(schema = @Schema(implementation = UserDTO.class))
-    )
-    @ApiResponse(
-            responseCode = "404",
-            description = "Not Found",
-            content = @Content(schema = @Schema(implementation = String.class))
-    )
-    public UserDTO getUserById(@Parameter(
-            description = "ID of the user",
-            required = true
-    ) @PathVariable Long id) {
+    @GetUserById(path = "/{id}")
+    public UserDTO getUserById(@PathVariable Long id) {
         return crudUserService.getById(id);
     }
 
-    @PostMapping
-    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
-    @ResponseStatus(HttpStatus.CREATED)
-    @ApiResponse(
-            responseCode = "201",
-            description = "Successful operation.",
-            content = @Content(schema = @Schema(implementation = UserDTO.class))
-    )
-    @ApiResponse(
-            responseCode = "400",
-            description = "Invalid data",
-            content = @Content(schema = @Schema(implementation = String.class))
-    )
-    @Operation(
-            summary = "Creation of new user.Available for ADMIN.",
-            description = "Creation of new user"
-    )
-    public UserCreateDTO createUser(@Valid
-                                    @io.swagger.v3.oas.annotations.parameters.RequestBody(
-                                            description = "User data, required for creation",
-                                            required = true,
-                                            content = @Content(schema = @Schema(implementation = UserCreateDTO.class))
-                                    ) @RequestBody UserCreateDTO userCreateDTO) {
+    @CreateUser(path = "")
+    public UserCreateDTO createUser(@Valid @RequestBody UserCreateDTO userCreateDTO) {
         return crudUserService.create(userCreateDTO);
     }
 
-    @PutMapping("/me/update")
-    @Operation(
-            summary = "Update my info: name, surname, phoneNumber, streetAndHouseNumber, city, postalCode.",
-            description = "Updates my details."
-    )
-    @ApiResponse(
-            responseCode = "200",
-            description = "Successful operation",
-            content = @Content(schema = @Schema(implementation = UserUpdateDTO.class))
-    )
+    @UpdateMe(path = "/me/update")
     public UserDTO updateUser(@Valid @RequestBody UserUpdateDTO userUpdateDTO, @AuthenticationPrincipal User user) {
         return crudUserService.update(userUpdateDTO, user.getId());
     }
 
-    @PutMapping("/me/update-email")
-    @Operation(
-            summary = "Update my email",
-            description = "Updates the email of an existing user and sends a confirmation email"
-    )
-    @ApiResponse(
-            responseCode = "200",
-            description = "Successful operation",
-            content = @Content(schema = @Schema(implementation = UserEmailDto.class))
-    )
+    @UpdateMyEmail(path = "/me/update-email")
     public void updateEmail(@Valid @RequestBody UserEmailDto userEmailDto, @AuthenticationPrincipal User user, HttpServletRequest request) {
         crudUserService.updateEmail(userEmailDto, user.getId(), request);
     }
 
-
-    @PutMapping("/me/update-password")
-    @Operation(
-            summary = "Update my password",
-            description = "Updates the password of an existing user"
-    )
-    @ApiResponse(
-            responseCode = "200",
-            description = "Successful operation",
-            content = @Content(schema = @Schema(implementation = PasswordUpdateDTO.class))
-    )
+    @UpdatePassword(path = "/me/update-password")
     public void updatePassword(@Valid @RequestBody PasswordUpdateDTO passwordUpdateDTO, @AuthenticationPrincipal User user) {
         crudUserService.updatePassword(passwordUpdateDTO, user.getId());
     }
 
-    @DeleteMapping
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    @ApiResponse(
-            responseCode = "200",
-            description = "Successful operation."
-    )
-    @ApiResponse(
-            responseCode = "204",
-            description = "No content present.",
-            content = @Content(schema = @Schema(implementation = String.class))
-    )
-    @Operation(
-            summary = "Delete my own account.",
-            description = "Deleting of user."
-    )
-    public void deleteUser(@Parameter(
-            description = "ID of the user",
-            required = true
-    ) @AuthenticationPrincipal User user) {
+    @DeleteUser(path = "")
+    public void deleteUser(@AuthenticationPrincipal User user) {
         crudUserService.delete(user.getId());
     }
-
 }
