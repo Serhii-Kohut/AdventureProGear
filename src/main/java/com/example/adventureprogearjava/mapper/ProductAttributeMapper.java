777cef8908aedbb66c583e83d23111bae1d5b609
Serhii Kohut <113467605+ronycv@users.noreply.github.com>
2024-09-16 21:15:31 +0300

    minor fix

--- src/main/java/com/example/adventureprogearjava/mapper/ProductAttributeMapper.java
+++ src/main/java/com/example/adventureprogearjava/mapper/ProductAttributeMapper.java
@@ -1,8 +1,6 @@
 package com.example.adventureprogearjava.mapper;
 
 import com.example.adventureprogearjava.dto.ProductAttributeDTO;
-import com.example.adventureprogearjava.dto.ProductDTO;
-import com.example.adventureprogearjava.entity.Product;
 import com.example.adventureprogearjava.entity.ProductAttribute;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
