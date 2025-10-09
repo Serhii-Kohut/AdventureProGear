d03db04b5d8b39263cfd46cf204d90d5c431f894
Serhii Kohut <113467605+ronycv@users.noreply.github.com>
2025-10-09 21:33:24 +0300

    removed unused imports

--- src/main/java/com/example/adventureprogearjava/config/MailConfig.java
+++ src/main/java/com/example/adventureprogearjava/config/MailConfig.java
@@ -2,15 +2,9 @@ package com.example.adventureprogearjava.config;
 
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.Properties;
 
 @Configuration
 @FieldDefaults(level = AccessLevel.PRIVATE)
